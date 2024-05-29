de234133b2327a1c1eab9e8995c594f540ec322d
Abel Diaz <abeldiaz2307@gmail.com>
2024-05-29 14:39:04 -0600

    First commit

---
+++ ProyectoDB2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProyectoDB2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
