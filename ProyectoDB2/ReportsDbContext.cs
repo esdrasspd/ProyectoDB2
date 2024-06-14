9da15fe5bfaaca667cab7e80cf0acb4876c0c56b
Alex <alex2000.sabe@gmail.com>
2024-06-14 14:23:50 -0600

    Otro DB context

---
+++ ProyectoDB2/ReportsDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProyectoDB2.DTOs;
+using ProyectoDB2.Models;
+
+namespace ProyectoDB2
+{
+	public class ReportsDbContext : DbContext
+	{
+		public ReportsDbContext(DbContextOptions<ReportsDbContext> options) : base(options)
+		{
+		}
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<TipoProductoDTO>().HasNoKey();
+			modelBuilder.Entity<ProductoDTO>().HasNoKey();
+			modelBuilder.Entity<ClienteDTO>().HasNoKey();
+			modelBuilder.Entity<ProductoDTO>().Property(p => p.Precio)
+				.HasColumnType("money");
+			modelBuilder.Entity<ProductModelSP>().HasKey(p => p.Referencia); // Configurar Referencia como clave primaria
+			modelBuilder.Entity<TipoClienteDTO>().HasNoKey();
+			modelBuilder.Entity<AdministradorDTO>().HasNoKey();
+			modelBuilder.Entity<UsuarioDTO>().HasNoKey();
+			modelBuilder.Entity<ReporteVentaDiariaDTO>().HasNoKey();
+			modelBuilder.Entity<ReporteProductoMasVendidoDTO>().HasNoKey();
+		}
+	}
+}
